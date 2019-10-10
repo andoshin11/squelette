8ec30d1ba8974aea7ec7f8f0d786c34a7ce09534
andoshin11 <shinglish11@gmail.com>
2019-10-10 13:45:20 +0900

    Enhance core types

--- packages/mill-core/src/types.ts
+++ packages/mill-core/src/types.ts
@@ -7,3 +7,103 @@ export interface TSSchema {
   enum: any[]
   properties: { [name: string]: TSSchema }
 }
+
+export type SwaggerSchemaFormat =
+  | 'int32'
+  | 'int64'
+  | 'uint64'
+  | 'float'
+  | 'double'
+  | 'byte'
+  | 'date'
+  | 'date-time'
+
+export type SwaggerSchemaType =
+  | 'Array'
+  | 'array'
+  | 'List'
+  | 'boolean'
+  | 'string'
+  | 'int'
+  | 'float'
+  | 'number'
+  | 'long'
+  | 'short'
+  | 'char'
+  | 'double'
+  | 'object'
+  | 'integer'
+  | 'Map'
+  | 'date'
+  | 'DateTime'
+  | 'binary'
+  | 'ByteArray'
+  | 'UUID'
+  | 'File'
+  | 'Error'
+
+
+// ------------------------------ Schema -------------------------------------
+export interface BaseSchema {
+  title?: string
+  multipleOf?: number
+  maximum?: number
+  exclusiveMaximum?: boolean
+  minimum?: number
+  exclusiveMinimum?: boolean
+  maxLength?: number
+  minLength?: number
+  pattern?: string
+  maxItems?: number
+  minItems?: number
+  uniqueItems?: boolean
+  maxProperties?: number
+  minProperties?: number
+  enum?: Array<string | boolean | number | {}>
+}
+
+export interface Schema extends BaseSchema {
+  type?: string
+  allOf?: Schema[]
+  oneOf?: Schema[]
+  anyOf?: Schema[]
+  not?: Schema[]
+  items?: Schema | Schema[]
+  properties?: { [propertyName: string]: Schema }
+  additionalProperties?: Schema
+  description?: string
+  format?:
+    | 'int32'
+    | 'int64'
+    | 'float'
+    | 'double'
+    | 'byte'
+    | 'binary'
+    | 'date'
+    | 'date-time'
+    | 'password'
+  default?: string | boolean | number | {}
+  nullable?: boolean
+  discriminator?: string
+  readOnly?: boolean
+  writeOnly?: boolean
+  xml?: XML
+  externalDocs?: ExternalDocs
+  example?: any
+  deprecated?: boolean
+  required?: string[]
+  $ref?: string
+}
+
+export interface XML {
+  name?: string
+  namespace?: string
+  prefix?: string
+  attribute?: boolean
+  wrapped?: boolean
+}
+
+export interface ExternalDocs {
+  url: string
+  description?: string
+}
