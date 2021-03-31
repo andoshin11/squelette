cad56a3cca9758afd80f1a483da041d1f6ff6f1d
andoshin11 <shinglish11@gmail.com>
2021-03-31 17:54:49 +0900

    request-gen: Refactor output code

--- packages/squelette-request-gen/example/requests/types.ts
+++ packages/squelette-request-gen/example/requests/types.ts
@@ -1,16 +1,15 @@
-export const HTTPMethod = {
-  get: "GET",
-  post: "POST",
-  delete: "DELETE",
-  put: "PUT"
-} as const;
-
-export type TupleToUnion<T> = T extends { [K in keyof T]: infer U } ? U : never;
-
 export interface APIRequest<R> {
   response: R;
   path: string;
-  method: TupleToUnion<typeof HTTPMethod>;
+  method:
+    | "ALL"
+    | "GET"
+    | "POST"
+    | "PUT"
+    | "DELETE"
+    | "OPTIONS"
+    | "HEAD"
+    | "PATCH";
   params?: any;
   baseURL?: string;
 }
--- packages/squelette-request-gen/src/helper.ts
+++ packages/squelette-request-gen/src/helper.ts
@@ -6,7 +6,7 @@ export const optionalSymbol = (schema: TSSchema): string => schema.isRequired ? 
 
 export const arraySymbol = (schema: TSSchema): string => schema.isArray ? '[]' : ''
 
-export const toTemplateLiteral = (path: string): string => path.replace(/\{/, "${pathParameter.")
+export const toTemplateLiteral = (path: string): string => path.replace(/\{/, "${args.pathParameter.")
 
 export const isVoid = (schema: TSSchema): boolean => {
   return schema.type === 'void' && !Object.keys(schema.properties).length
